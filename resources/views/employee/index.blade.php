67b180fc51f48c1b95966b630cbc7ea0a8b834a5
Santiago <sxntydev@gmail.com>
2023-06-07 15:12:11 -0300

    feat added style and check if employee is empty

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -11,35 +11,57 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
     </script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+
 </head>
 
 <body>
-    <table class="table table-light">
-        <thead class="thead-light">
-            <tr>
-                <th>#</th>
-                <th>Photo</th>
-                <th>Name</th>
-                <th>Surname</th>
-                <th>Email</th>
-                <th>Phone Number</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($employees as $employee)
+    <main class="m-5">
+        <h1 class="my-5">Employees list</h1>
+        <table class="table table-light">
+            <thead class="thead-light">
                 <tr>
-                    <td>{{ $employee->id }}</td>
-                    <td>{{ $employee->photo }}</td>
-                    <td>{{ $employee->firstname }}</td>
-                    <td>{{ $employee->surname }}</td>
-                    <td>{{ $employee->email }}</td>
-                    <td>{{ $employee->phoneNumber }}</td>
-                    <td>Edit | Delete</td>
+                    <th>#</th>
+                    <th>Photo</th>
+                    <th>Name</th>
+                    <th>Surname</th>
+                    <th>Email</th>
+                    <th>Phone Number</th>
+                    <th>Actions</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+
+                @foreach ($employees as $employee)
+                    <tr>
+                        <td>{{ $employee->id }}</td>
+                        <td>{{ $employee->photo }}</td>
+                        <td>{{ $employee->firstname }}</td>
+                        <td>{{ $employee->surname }}</td>
+                        <td>{{ $employee->email }}</td>
+                        <td>{{ $employee->phoneNumber }}</td>
+                        <td>Edit |
+
+                            <form action="{{ url('/employee/' . $employee->id) }}" method="POST">
+                                @csrf
+                                {{ method_field('DELETE') }}
+                                <input type="submit" value="Delete" class="btn btn-danger">
+                            </form>
+
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @if ($employees->isEmpty())
+            <div class="alert alert-danger d-flex align-items-center justify-content-between" role="alert">
+                <p class="m-0">You dont have any employee.</p>
+                <form action="{{ url('/employee/create') }}" method="GET">
+                    <button type="submit" class="btn btn-success mx-1"><i class="bi bi-plus-circle"></i></button>
+                </form>
+            </div>
+        @endif
+    </main>
 </body>
 
 </html>
