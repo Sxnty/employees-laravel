9e464bd632e86fa9281cd53e6365cb2e8473d44d
Santiago <sxntydev@gmail.com>
2023-06-07 16:40:00 -0300

    checks if employe exist before render

--- resources/views/employee/form.blade.php
+++ resources/views/employee/form.blade.php
@@ -1,16 +1,16 @@
 <label for="firstname">Name</label>
-<input type="text" name="firstname" id="" />
+<input type="text" name="firstname" id="" value="{{ isset($employee) ? $employee->firstname : '' }}"/>
 
 <label for="surname">Surname</label>
-<input type="text" name="surname" id="" />
+<input type="text" name="surname" id="" value="{{ isset($employee) ? $employee->surname : '' }}"/>
 
 <label for="email">Email</label>
-<input type="email" name="email" id="" />
+<input type="email" name="email" id="" value="{{ isset($employee) ? $employee->email : '' }}"/>
 
 <label for="phoneNumber">Phone Number</label>
-<input type="number" name="phoneNumber" id="" />
+<input type="number" name="phoneNumber" id="" value="{{ isset($employee) ? $employee->phoneNumber : '' }}"/>
 
 <label for="photo">Photo</label>
-<input type="file" name="photo" id="" />
+<input type="file" name="photo" id="" value="{{ isset($employee) ? $employee->photo : '' }}"/>
 
 <input type="submit" value="Add employee">
