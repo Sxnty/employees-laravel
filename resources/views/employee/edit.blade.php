357350fc42c2ecee9dc2e576b4fed3b813dad9db
Santiago <sxntydev@gmail.com>
2023-06-07 17:47:38 -0300

    style things

--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -1,5 +1,22 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+</script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+
+<main class="w-50 h-100 d-flex align-items-center justify-content-center flex-column m-auto">
+    <h1>Edit Employee Data</h1>
 <form action="{{url('employee/'.$employee->id)}}" method="POST" enctype="multipart/form-data">
     @csrf
     @method('PATCH')
     @include('employee.form')
 </form>
+
+</main>
--- resources/views/employee/form.blade.php
+++ resources/views/employee/form.blade.php
@@ -1,16 +1,33 @@
-<label for="firstname">Name</label>
-<input type="text" name="firstname" id="" value="{{ isset($employee) ? $employee->firstname : '' }}"/>
+<div class="form-group">
+    <label for="firstname">Name</label>
+    <input class="form-control" placeholder="Enter name" type="text" name="firstname" id=""
+        value="{{ isset($employee) ? $employee->firstname : '' }}" />
+</div>
 
-<label for="surname">Surname</label>
-<input type="text" name="surname" id="" value="{{ isset($employee) ? $employee->surname : '' }}"/>
+<div class="form-group">
+    <label for="surname">Surname</label>
+    <input class="form-control" placeholder="Enter Surname" type="text" name="surname" id=""
+        value="{{ isset($employee) ? $employee->surname : '' }}" />
+</div>
 
-<label for="email">Email</label>
-<input type="email" name="email" id="" value="{{ isset($employee) ? $employee->email : '' }}"/>
+<div class="form-group">
+    <label for="email">Email</label>
+    <input class="form-control" placeholder="Enter email" type="email" name="email" id=""
+        value="{{ isset($employee) ? $employee->email : '' }}" />
+</div>
 
-<label for="phoneNumber">Phone Number</label>
-<input type="number" name="phoneNumber" id="" value="{{ isset($employee) ? $employee->phoneNumber : '' }}"/>
+<div class="form-group">
+    <label for="phoneNumber">Phone Number</label>
+    <input class="form-control" placeholder="Enter phone number" type="number" name="phoneNumber" id=""
+        value="{{ isset($employee) ? $employee->phoneNumber : '' }}" />
+</div>
 
-<label for="photo">Photo</label>
-<input type="file" name="photo" id="" value="{{ isset($employee) ? $employee->photo : '' }}"/>
-
-<input type="submit" value="Add employee">
+<div class="form-group">
+    <label for="photo">Photo</label>
+    <input class="form-control" type="file" name="photo" id=""
+        value="{{ isset($employee) ? $employee->photo : '' }}" />
+</div>
+<div class="d-flex w-100  align-items-center">
+    <input type="submit" value="Add employee" class="btn btn-primary mt-5 ">
+    <a href="{{ url('/employee') }}" class="btn btn-secondary d-flex align-items-center justify-content-center mt-5 mx-5 ">Go Back</a>
+</div>
