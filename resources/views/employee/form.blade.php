3998441e5dae06645339a3638bec9d5ce25ea019
Santiago <sxntydev@gmail.com>
2023-06-07 15:28:36 -0300

    refactor

--- resources/views/employee/form.blade.php
+++ resources/views/employee/form.blade.php
@@ -1,1 +1,16 @@
-form that contains edit and create data
+<label for="firstname">Name</label>
+<input type="text" name="firstname" id="" />
+
+<label for="surname">Surname</label>
+<input type="text" name="surname" id="" />
+
+<label for="email">Email</label>
+<input type="email" name="email" id="" />
+
+<label for="phoneNumber">Phone Number</label>
+<input type="number" name="phoneNumber" id="" />
+
+<label for="photo">Photo</label>
+<input type="file" name="photo" id="" />
+
+<input type="submit" value="Add employee">
