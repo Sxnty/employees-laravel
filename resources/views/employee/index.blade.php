578eb9a8975d2f0171affe45323431e7eaf40f76
Santiago <sxntydev@gmail.com>
2023-06-07 16:40:18 -0300

    some styling

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -23,8 +23,13 @@
 
 <body>
     <main class="m-5">
-        <h1 class="my-5">Employees list</h1>
-        <table class="table table-light">
+        <div class="d-flex align-items-center justify-content-between">
+            <h1 class="my-5">Employees list</h1>
+            <form action="{{ url('/employee/create') }}" method="GET">
+                <button type="submit" class="btn btn-primary mx-1">Add employee</button>
+            </form>
+        </div>
+        <table class="table table-light ">
             <thead class="thead-light">
                 <tr>
                     <th>#</th>
@@ -39,9 +44,11 @@
             <tbody>
 
                 @foreach ($employees as $employee)
-                    <tr>
+                    <tr d-flex class="align-items-center">
                         <td>{{ $employee->id }}</td>
-                        <td>{{ $employee->photo }}</td>
+                        <td>
+                            <img src="{{ asset('storage') . '/' . $employee->photo }}" alt="" style="width: 8rem" class="rounded">
+                        </td>
                         <td>{{ $employee->firstname }}</td>
                         <td>{{ $employee->surname }}</td>
                         <td>{{ $employee->email }}</td>
@@ -55,6 +62,7 @@
                                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                                     <form action="{{ url('/employee/' . $employee->id . '/edit') }}" method="GET">
                                         @csrf
+                                        @method('PATCH')
                                         <input type="submit" value="Edit" class="btn btn-link ">
                                     </form>
                                     <form action="{{ url('/employee/' . $employee->id) }}" method="POST">
